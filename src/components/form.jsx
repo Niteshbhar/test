29ddd7e152547e45e6bf06330e41dd10ff23a258
NITESH BHARDWAJ <niteshbhardwaj2001@gamil.com>
2024-11-09 23:37:18 +0530

    form data

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Form() {
   return (
     <>
-      <section className="bg-gray-100  dark:bg-gray-900" >
+      <section className="bg-gray-100  dark:bg-gray-900">
         <div className=" flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           {/* <a
           href="#"
@@ -17,7 +17,7 @@ function Form() {
           Flowbite
         </a> */}
           <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Create Employee
               </h1>
@@ -75,99 +75,82 @@ function Form() {
                     for="Designation"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Designation
+                    Designation :
+                    <select
+                      className="px-2"
+                      name="Designation"
+                      id="designation"
+                    >
+                      <option value="HR">Select option</option>
+                      <option value="HR">HR</option>
+                      <option value="Manager">Manager</option>
+                      <option value="sales">sales</option>
+                    </select>
                   </label>
-                  <select name="Designation" id="designation">
-                    <option  value="HR">
-                      Select option
-                    </option>
-                    <option value="HR">HR</option>
-                    <option value="Manager">Manager</option>
-                    <option value="sales">sales</option>
-                  </select>
                 </div>
                 <div>
                   <label
                     for="email"
                     className=" block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Gender 
-                    <input className="ml-3"
-                    type="radio"
-                    id="male"
-                    name="fav_language"
-                    value="HTML"
-                  />
-                  <label className="" for="html">Male</label>
-                 
-                  <input
-                  className="ml-3"
-                    type="radio"
-                    id="female"
-                    name="fav_language"
-                    value="CSS"
-                  />
-                  <label for="css">Female</label>
-                  <br />
-                  </label>
-                 
-                </div>
-                <div>
-                  <label
-                    for="confirm-password"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Confirm password
-                  </label>
-                  <input
-                    type="confirm-password"
-                    name="confirm-password"
-                    id="confirm-password"
-                    placeholder="••••••••"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                  />
-                </div>
-                <div class="flex items-start">
-                  <div class="flex items-center h-5">
+                    Gender :
                     <input
-                      id="terms"
-                      aria-describedby="terms"
-                      type="checkbox"
-                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required=""
+                      className="ml-3"
+                      type="radio"
+                      id="male"
+                      name="fav_language"
+                      value="HTML"
                     />
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label
-                      for="terms"
-                      class="font-light text-gray-500 dark:text-gray-300"
-                    >
-                      I accept the{" "}
-                      <a
-                        class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                        href="#"
-                      >
-                        Terms and Conditions
-                      </a>
+                    <label className="" for="html">
+                      Male
                     </label>
-                  </div>
+                    <input
+                      className="ml-3"
+                      type="radio"
+                      id="female"
+                      name="fav_language"
+                      value="CSS"
+                    />
+                    <label for="css">Female</label>
+                    <br />
+                  </label>
                 </div>
-                <button
-                  type="submit"
-                  class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                >
-                  Create an account
-                </button>
-                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Already have an account?{" "}
-                  <Link
-                    to="/"
-                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                <div className="">
+                  <label
+                    for="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Login here
-                  </Link>
-                </p>
+                    Course :
+                    <input
+                      className="ml-4"
+                      type="checkbox"
+                      id="vehicle1"
+                      name="vehicle1"
+                      value="Bike"
+                    />
+                    <label for="vehicle1"> MCA</label>
+                    <input
+                      className="ml-4"
+                      type="checkbox"
+                      id="vehicle1"
+                      name="vehicle1"
+                      value="Bike"
+                    />
+                    <label for="vehicle1"> BCA</label>
+                    <input
+                      className="ml-4"
+                      type="checkbox"
+                      id="vehicle1"
+                      name="vehicle1"
+                      value="Bike"
+                    />
+                    <label for="vehicle1"> BSC</label>
+                  </label>
+                </div>
+                <div className="w-100">
+                <label className="w-100" for="img">Select image: <input type="file" id="img" name="img" accept="image/*"/></label>
+               
+                </div>
               </form>
             </div>
           </div>
