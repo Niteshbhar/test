79579a204cc5bcc1eaa0daaf492bb7da07415d2f
NITESH BHARDWAJ <niteshbhardwaj2001@gamil.com>
2024-11-09 23:15:07 +0530

    changes

---
+++ src/components/form.jsx
@@ -0,0 +1,180 @@
+import { Link } from "react-router-dom";
+
+function Form() {
+  return (
+    <>
+      <section className="bg-gray-100  dark:bg-gray-900" >
+        <div className=" flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          {/* <a
+          href="#"
+          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img
+            class="w-8 h-8 mr-2"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            alt="logo"
+          />
+          Flowbite
+        </a> */}
+          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Create Employee
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    for="name"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="name"
+                    name="name"
+                    id="name"
+                    placeholder="Full Name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Mobile No
+                  </label>
+                  <input
+                    type="tel"
+                    name="mobile"
+                    id="mobile"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="99XXXXX"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    for="Designation"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Designation
+                  </label>
+                  <select name="Designation" id="designation">
+                    <option  value="HR">
+                      Select option
+                    </option>
+                    <option value="HR">HR</option>
+                    <option value="Manager">Manager</option>
+                    <option value="sales">sales</option>
+                  </select>
+                </div>
+                <div>
+                  <label
+                    for="email"
+                    className=" block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Gender 
+                    <input className="ml-3"
+                    type="radio"
+                    id="male"
+                    name="fav_language"
+                    value="HTML"
+                  />
+                  <label className="" for="html">Male</label>
+                 
+                  <input
+                  className="ml-3"
+                    type="radio"
+                    id="female"
+                    name="fav_language"
+                    value="CSS"
+                  />
+                  <label for="css">Female</label>
+                  <br />
+                  </label>
+                 
+                </div>
+                <div>
+                  <label
+                    for="confirm-password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    type="confirm-password"
+                    name="confirm-password"
+                    id="confirm-password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+                <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required=""
+                    />
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label
+                      for="terms"
+                      class="font-light text-gray-500 dark:text-gray-300"
+                    >
+                      I accept the{" "}
+                      <a
+                        class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                        href="#"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </label>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Create an account
+                </button>
+                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Already have an account?{" "}
+                  <Link
+                    to="/"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Login here
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Form;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import SignUp from "../signup/signUp";
 import Login from "../login/login";
+import Form from "../components/form";
 
 function Router() {
     return ( <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Form/>} />
         <Route path="/signup" element={<SignUp/>} />
       </Routes>  );
 }
